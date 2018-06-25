2efc6a2e0295ed6852d8d6c60ca3741c87d038c4
todaylg <lg2lugang@gmail.com>
2018-06-26 05:37:30 +0800

    Fix Header Css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -170,7 +170,8 @@ class Header extends React.Component {
           @from-width desktop {
             .header {
               align-items: center;
-              background-color: ${theme.color.neutral.white};
+              // background-color: ${theme.color.neutral.white};
+              background-color: rgba(255,255,255,0);
               display: flex;
               position: absolute;
               top: 0;
