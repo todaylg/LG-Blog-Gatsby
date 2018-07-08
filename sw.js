f968324ad48a942393c11c685de60efdebe46bb3
todaylg <lg2lugang@gmail.com>
2018-07-08 14:26:18 +0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/LG-Blog-Gatsby/app-241e8ef7613c45b7bf3d.js","460f2efe405fdefd82d01701f7682fff"],["/LG-Blog-Gatsby/app-6660df3e50d220053e06.js","861814b7271fd22633d95242a4df5d44"],["/LG-Blog-Gatsby/app-9006f7cbfc190ef576b7.js","d830a55032d74a056b1b4d9f2e8d6cba"],["/LG-Blog-Gatsby/app-df4fe1beb612c25c9595.js","1d724e4b1e350a7482fe0282e04a8e2a"],["/LG-Blog-Gatsby/commons-7c4d77f391b491c8192f.js","94559ccb8ac61a43f6a4acb506650d62"],["/LG-Blog-Gatsby/index.html","c3f4c3a685d389806b6f78fe2e76d97d"],["/LG-Blog-Gatsby/manifest.webmanifest","8d87117c863bc4e503d71f4c41cf56c4"],["/LG-Blog-Gatsby/offline-plugin-app-shell-fallback/index.html","669eb57cad00d12ff513df6d57d23935"]];
+var precacheConfig = [["/LG-Blog-Gatsby/app-241e8ef7613c45b7bf3d.js","460f2efe405fdefd82d01701f7682fff"],["/LG-Blog-Gatsby/app-4271df83f97ee45a72db.js","7083e8b6f1e1a3c55202e96018bb0874"],["/LG-Blog-Gatsby/app-6660df3e50d220053e06.js","861814b7271fd22633d95242a4df5d44"],["/LG-Blog-Gatsby/app-9006f7cbfc190ef576b7.js","d830a55032d74a056b1b4d9f2e8d6cba"],["/LG-Blog-Gatsby/app-df4fe1beb612c25c9595.js","1d724e4b1e350a7482fe0282e04a8e2a"],["/LG-Blog-Gatsby/commons-7c4d77f391b491c8192f.js","acbcc1adfe9f8321d0f3150e42bf88e4"],["/LG-Blog-Gatsby/index.html","0b270c928a666011805a82e4a4d1f7c5"],["/LG-Blog-Gatsby/manifest.webmanifest","8d87117c863bc4e503d71f4c41cf56c4"],["/LG-Blog-Gatsby/offline-plugin-app-shell-fallback/index.html","27d7458be976d7d2732bd6c89e64be88"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
