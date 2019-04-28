80b535cddf9a20b4c260102cf89f1abd3933ac69
todaylg <lg2lugang@gmail.com>
2019-04-28 17:46:05 +0800

    Updates

--- path---category-废话集-f8c32bea377b2831e9c2.js
+++ path---category-废话集-f8c32bea377b2831e9c2.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x78934b31c1b2],{400:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/miku-setup/"},excerpt:"中了Miku的毒。。。希望明年公主殿下的生日可以搂一个炫酷的页面给她~Web3D搂起啊！！",timeToRead:1,frontmatter:{title:"MikuMikuMi",category:"废话集"}}}]},site:{siteMetadata:{facebook:{appId:""}}}},pathContext:{category:"废话集"}}}});
+webpackJsonp([0x78934b31c1b2],{401:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/miku-setup/"},excerpt:"中了Miku的毒。。。希望明年公主殿下的生日可以搂一个炫酷的页面给她~Web3D搂起啊！！",timeToRead:1,frontmatter:{title:"MikuMikuMi",category:"废话集"}}}]},site:{siteMetadata:{facebook:{appId:""}}}},pathContext:{category:"废话集"}}}});
 //# sourceMappingURL=path---category-废话集-f8c32bea377b2831e9c2.js.map
--- path---test-5c4db413742bf71a7154.js
+++ path---test-5c4db413742bf71a7154.js
@@ -1,2 +1,2 @@
-webpackJsonp([89450997894629],{419:function(t,e){t.exports={data:{page:{id:"/Users/lugang/My Github/LG-Blog-Gatsby/content/pages/1--test/index.md absPath of file >>> MarkdownRemark",html:"",frontmatter:{title:"Test"}},site:{siteMetadata:{facebook:{appId:""}}}},pathContext:{slug:"/test/"}}}});
+webpackJsonp([89450997894629],{420:function(t,e){t.exports={data:{page:{id:"/Users/lugang/My Github/LG-Blog-Gatsby/content/pages/1--test/index.md absPath of file >>> MarkdownRemark",html:"",frontmatter:{title:"Test"}},site:{siteMetadata:{facebook:{appId:""}}}},pathContext:{slug:"/test/"}}}});
 //# sourceMappingURL=path---test-5c4db413742bf71a7154.js.map
